ee6d1b75a2cbf2a7463c9d6fa43de1429d6dca5a
Priyansh Thakar <priyanshthakar@gmail.com>
2024-02-06 18:57:59 -0500

    functions complete

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,24 +1,49 @@
 /* STEP 1: Basic browser functions
 		There are a number of functions that are included with most modern browsers - the replace() function is just one of many */
+var step1a = "Hello! How are you?";
+var step1b = step1a.replace("are", "is");
+console.log(step1a);
+console.log(step1b);
 
 // Interestingly, functions that are included with your browser are technically called 'methods' rather than functions.
 
 /* STEP 2: Anonymous versus named functions */
 var step2Button = document.querySelector('#step2');
+
 // Anonymous
+step2Button.addEventListener("click", function () {
+    alert("I am in an anonymous function.");
+})
 
 // Named function
+step2Button.addEventListener("click", namedFun);
+
+function namedFun() {
+    alert("I am in a named function.");
+}
 
 /* STEP 3: Function parameters
 Some methods or functions don't require any parameters (sometimes called arguments, properties, or attributes), while some do, 
 and others are optional. */
 
 // No parameters needed
+function addNumbers() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
 
 // Two parameters needed
+function addNumbersTwo(x, y) {
+    console.log(x + y);
+}
 
 // Parameters optional
 // join
+var myArray = ["apple", "banana", "grapes"];
+console.log(myArray.join());
+console.log(myArray.join(" - "));
+console.log(myArray.join(" ---------------- "));
 
 // Next, open up scope.html and we will look at how the accessibility of variables depends on where they are defined/initialized.
 
